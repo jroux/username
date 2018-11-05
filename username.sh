a939066b9c94fc73a5541732d975d05a0f8ac343
Jessie Roux <44593518+jroux@users.noreply.github.com>
2018-11-05 11:48:15 -0800

    Update username.sh

--- username.sh
+++ username.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 # username.sh
-# Jessie Roux
-echo "Enter a username with lowercase letters and between 3-12 characters: "
+# Jessica Roux
+echo "Please enter a username beginning with a lowercase letter and between 3-12 characters. The username can be made up of lowercase letters, numbers and underscores: "
 read USERNAME
 while echo $USERNAME | egrep -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username. Please try again."
-	echo "Enter a valid username with lowercase letters and between 3-12 characters: "
+	echo "Enter a valid username starting with a lowercase letter and between 3-12 characters. The username can be made up of lowercase letters, numbers and underscores: " 
 	read USERNAME
 done
-echo "Thank you"
+echo "Thank you!"
 
